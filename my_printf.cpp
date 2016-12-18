a6c4b9d450b92211de1552699e4f1ed101a7dbe0
liangshaocong <lsc19941028@gmail.com>
2016-12-18 19:43:08 +0800

    my_printf

--- my_printf.cpp
+++ my_printf.cpp
@@ -1,13 +1,53 @@
 //
 // Created by mark on 16-12-8.
 //
+// printf implement with variadic template
 #include <iostream>
+#include <set>
 
-template <typename T, typename ...Args>
-void my_printf(const char *fmt, Args ... args){
+#include <string.h>
 
-};
+namespace liangsc_toys {
+    using std::cout;
+    const std::set<char> tag = {'d', 'c', 'f', 'u', 'p'};
 
-int main(int argc, char **argv){
+    void my_printf(const char *fmt) {
+        int len = strlen(fmt);
+        for(int i = 0; i < len; ++i){
+            if(fmt[i] == '%') {
+                if (i + 1 < len && fmt[i + 1] == '%')
+                    ++i;
+                else
+                    throw std::runtime_error("Invalid format string: missing format arguments");
+            }
+            cout << fmt[i];
+        }
+    }
+
+    template<typename T, typename ...Args>
+    void my_printf(const char *fmt, const T value, Args ...args) {
+        int len = strlen(fmt);
+        for(int i = 0; i < len; ++i){
+            if(fmt[i] == '%'){
+                if(!(i+1 < len))
+                    return ;
+                else{
+                    if(fmt[i+1] == '%') {
+                        ++i;
+                    }
+                    else if(tag.find(fmt[i+1]) != tag.end()){
+                        cout << value;
+                        my_printf(fmt+2, args...);
+                        return;
+                    }
+                }
+            }
+            cout << fmt[i];
+        }
+    }
+}
+using namespace liangsc_toys;
+int main(void){
+    my_printf("%c\n", 'f');
     return 0;
 }
